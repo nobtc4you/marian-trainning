8e12d75d3e7c6e8a1f93ab7623859bc335463ae6
mariano <marceloguirras29@gmail.com>
2019-01-10 22:17:48 +0100

    calculator

---
+++ calculadora/main.js
@@ -0,0 +1,30 @@
+function sumar (a,b) {
+ document.getElementById('resultado').value = ""+(Number(a)+Number(b));
+}
+function restar (a,b){
+ document.getElementById('resultado').value = ""+(Number(a)-Number(b));
+}
+function multiplicar (a,b){
+ document.getElementById('resultado').value = ""+(Number(a)*Number(b));
+}
+function dividir (a,b){
+ document.getElementById('resultado').value = ""+(Number(a)/Number(b));
+}
+
+
+
+
+function reset() {
+       document.getElementById("a").value = "";
+       document.getElementById("b").value = "";
+       document.getElementById("resultado").value = "";
+
+   }
+
+/*
+   function supersuma(){
+var suma = a + b
+retur
+
+   }
+*/
